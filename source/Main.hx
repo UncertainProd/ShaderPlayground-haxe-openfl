9763d3871e8407ab0f9c000e4478752e3f54ddf1
UncertainProd <83609901+UncertainProd@users.noreply.github.com>
2023-01-29 16:36:37 +0530

    Flixel 5 change(s)

--- source/Main.hx
+++ source/Main.hx
@@ -1,15 +1,13 @@
 package;
 
 import flixel.FlxGame;
-import openfl.Lib;
 import openfl.display.Sprite;
-import openfl.events.Event;
 
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, true));
+		addChild(new FlxGame(0, 0, PlayState));
 	}
 }
